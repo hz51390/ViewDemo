94162b1cb759e4c3411fee01e07def80d3e2a739
hz <2506070216@qq.com>
2018-03-26 22:57:22 +0800

    init pro & view_demo

--- app/src/main/java/com/hz/demo/acttivity/MainActivity.java
+++ app/src/main/java/com/hz/demo/acttivity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.hz.demo;
+package com.hz.demo.acttivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.hz.demo.R;
 
 public class MainActivity extends AppCompatActivity {
 
