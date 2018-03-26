b37c54431a8dbb1b93e636e7d1ac161cdd250f52
hz <2506070216@qq.com>
2018-03-26 23:08:19 +0800

    add my_list_view

---
+++ app/src/main/java/com/hz/demo/widget/MyListView.java
@@ -0,0 +1,23 @@
+package com.hz.demo.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by Administrator on 2018/3/26 0026.
+ */
+
+public class MyListView extends ListView {
+    public MyListView(Context context) {
+        super(context);
+    }
+
+    public MyListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
