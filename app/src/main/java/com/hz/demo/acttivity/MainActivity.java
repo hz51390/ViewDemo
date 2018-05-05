e67b8fe2740d677cce14c766341e8fe1a8708efb
黄志 <huangzhi@huangzhideMacBook-Pro.local>
2018-05-05 10:41:48 +0800

    test

--- app/src/main/java/com/hz/demo/acttivity/MainActivity.java
+++ app/src/main/java/com/hz/demo/acttivity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hz.demo.acttivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +8,26 @@ import com.hz.demo.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String ID="MAIN_ID";
+    public static String NAME="MAIN_NAME";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    /**
+     * 在目标页面统一定义param，避免传参页面统一格式
+     * @param id
+     * @param name
+     * @return
+     */
+    public static Intent open(int id, String name){
+        Intent intent=new Intent();
+        intent.putExtra(ID,id);
+        intent.putExtra(NAME,name);
+        return new Intent();
     }
 }
